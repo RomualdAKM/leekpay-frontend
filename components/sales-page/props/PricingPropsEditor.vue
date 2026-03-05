52f102c91fdfbea45718f6784b6c07534a198d81
RomualdAKM <hope91303142@gmail.com>
2026-03-05 08:39:51 +0100

    new commit

--- components/sales-page/props/PricingPropsEditor.vue
+++ components/sales-page/props/PricingPropsEditor.vue
@@ -195,7 +195,7 @@
       </div>
     </div>
     
-    
+    <!-- ===== STYLE ===== -->
     <div class="border-b border-gray-200 pb-4">
       <button @click="sections.style = !sections.style" class="flex items-center justify-between w-full text-left">
         <h4 class="text-xs font-semibold text-gray-700 uppercase tracking-wider">Style</h4>
